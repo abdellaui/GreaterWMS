b033690301036bdda962f919381aeaf974f21594
abdellaui <abdullah.sahin@dwpbank.de>
2023-02-02 07:18:42 +0100

    fix: change port to 8086 while render.com doenst support it on free plan

--- backend_start.sh
+++ backend_start.sh
@@ -2,4 +2,4 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 #daphne -b 0.0.0.0 -p 8008 greaterwms.asgi:application
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8066
